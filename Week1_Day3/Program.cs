0e6a17de93d1f3f79c3be94b951d264d210683fe
razcrisan <rcrisan13@gmail.com>
2017-09-27 13:38:13 -0400

    conditional update

--- Week1_Day3/Program.cs
+++ Week1_Day3/Program.cs
@@ -26,24 +26,57 @@ namespace Week1_Day3
             //    Console.WriteLine("That Makes sense.");
             //}
 
-            Console.WriteLine("What day is today?");
-            string userAnswer = Console.ReadLine();
+            //Console.WriteLine("What day is today?");
+            //string userAnswer = Console.ReadLine();
 
-            if (userAnswer == "Monday")
+            //if (userAnswer == "Monday")
+            //{
+            //    Console.WriteLine("Back to work.");
+            //}
+
+            //else if (userAnswer == "Friday")
+            //{
+            //    Console.WriteLine("TGIF!!!!!!!");
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("Bloop");
+            //}
+
+            Console.WriteLine("How old are you?");
+            int userAge = int.Parse(Console.ReadLine());
+
+            if (userAge >= 0 && userAge <= 1)
             {
-                Console.WriteLine("Back to work.");
+                Console.WriteLine("Just a baby!");
             }
 
-            else if (userAnswer == "Friday")
+            else if (userAge > 1 && userAge < 4)
+
             {
-                Console.WriteLine("TGIF!!!!!!!");
+                Console.WriteLine("Yay Toddlers");
             }
 
+            else if (userAge >= 4 && userAge < 13)
+
+            {
+                Console.WriteLine("Enjoy your childhood");
+            }
+            else if (userAge >= 13 && userAge < 65)
+
+            {
+                Console.WriteLine("Life is Awkward");
+            }
+
+            else if (userAge >= 65 && userAge < 100)
+            {
+                Console.WriteLine("Enjoy your retirement.");
+            }
             else
             {
-                Console.WriteLine("Bloop");
+                Console.WriteLine("Thats not really your age.");
             }
-
 
 
 
