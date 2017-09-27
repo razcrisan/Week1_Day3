ea7bebbf70241a30469d2937ecbd1293c27882e2
razcrisan <rcrisan13@gmail.com>
2017-09-27 13:08:58 -0400

    Started with if-else

--- Week1_Day3/Program.cs
+++ Week1_Day3/Program.cs
@@ -10,6 +10,43 @@ namespace Week1_Day3
     {
         static void Main(string[] args)
         {
+            //int firstNumber = 2;
+            //int secondNumber = 6;
+
+            //if (firstNumber > secondNumber)
+            //{
+            //    //Do something
+            //    Console.WriteLine("You have broken math.");
+
+            //}
+
+            //else
+            //{
+            //    //Do something else
+            //    Console.WriteLine("That Makes sense.");
+            //}
+
+            Console.WriteLine("What day is today?");
+            string userAnswer = Console.ReadLine();
+
+            if (userAnswer == "Monday")
+            {
+                Console.WriteLine("Back to work.");
+            }
+
+            else if (userAnswer == "Friday")
+            {
+                Console.WriteLine("TGIF!!!!!!!");
+            }
+
+            else
+            {
+                Console.WriteLine("Bloop");
+            }
+
+
+
+
         }
     }
 }
